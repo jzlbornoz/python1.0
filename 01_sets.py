c0355b44d3caf3abf326a17e9a0fcc5d4a6d3aad
Javier Albornoz <90879621+jzlbornoz@users.noreply.github.com>
2024-03-19 00:42:11 +0000

    [update]

--- 01_sets.py
+++ 01_sets.py
@@ -1,26 +1,31 @@
-set_teams = {'barca', 'madrid', 'chelsea'}
+set_teams = {'barca', 'madrid', 'chelsea', 'estudiantes'}
 
 set_wc_years = {22, 18, 14, 10, 6, 2}
 
 set_goals = {20, '12', 10, '8', 6, '4', '2'}
 
-print(set_teams)
-print(type(set_teams))
-print(set_wc_years)
-print(set_goals)
-
 set_from_string = set('El campeon es argentina')
 
-print(set_from_string)
-
 set_from_tupla = set(('abc', 'def', 'ghi'))
-
-print(set_from_tupla)
 
 numbers = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 44, 44, 22, 22, 1, 1, 1, 3, 3, 3, 555
 ]
-print(numbers)
 
 uniqueNumbersArray = list(set(numbers))
 print(uniqueNumbersArray)
+
+#add
+set_teams.add('villa')
+set_teams.add('liverpool')
+
+#update  
+set_teams.update({'girona' , 'atm'})
+
+#remove
+set_teams.remove('madrid')
+set_teams.discard('bayern')
+set_teams.discard('estudiantes')
+#set_teams.remove('bayern') Error becouse not exist in the set
+
+print(set_teams)
